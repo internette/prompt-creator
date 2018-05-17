245cee26cff2c72f905130fe0b3e1a626b8cc093
Antoinette C. Janus <acjanus@pbs.org>
2018-05-17 16:46:54 -0400

    Refactored to have list scroll

--- src/components/presenters/creator.js
+++ src/components/presenters/creator.js
@@ -9,18 +9,18 @@ import '../../styles/creator.css';
 const CreatorPresenter = (props) => {
   return (
     <div id="creator">
-        <h1><span>Prompt Creator</span><button id="get-code">&lt;/&gt;</button><button id="how-to">?</button></h1>
-        <div>
-          <ul id="bash-list-items">
-            {
-              props.list_items.map((list_item, index)=> {
-                return <ListItem key={index} attributes={list_item}/>
-              })
-            }
-          </ul>
-          <AddInput/>
-        </div>
+      <h1><span>Prompt Creator</span><button id="get-code">&lt;/&gt;</button><button id="how-to">?</button></h1>
+      <div id="bash-list-container">
+        <ul id="bash-list-items">
+          {
+            props.list_items.map((list_item, index)=> {
+              return <ListItem key={index} attributes={list_item}/>
+            })
+          }
+        </ul>
       </div>
+      <AddInput/>
+    </div>
   );
 }
 
