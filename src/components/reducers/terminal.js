c93d6b9415a67730beea66c12e6cde8818d8950e
Antoinette C. Janus <acjanus@pbs.org>
2018-05-17 13:22:25 -0400

    Added update logic to terminal

--- src/components/reducers/terminal.js
+++ src/components/reducers/terminal.js
@@ -11,6 +11,17 @@ const terminal = (state = { parts: [] }, action)=> {
       return Object.assign({}, state, {
         parts: parts
       })
+    case 'update-item':
+      const updated_parts = state.parts.slice(0).map((part)=> {
+        if(part.id === action.item_to_update.id) {
+          return Object.assign({}, part, action.item_to_update);
+        } else {
+          return part
+        }
+      });
+      return Object.assign({}, state, {
+        parts: updated_parts
+      })
     case 'remove-item': 
       let without_removed_item = state.parts.filter((part)=> {
         if(part.id !== action.item_to_remove.id){
